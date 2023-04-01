31b0fd4f51e18a581b62f645c1022ebc0fe0dd46
AzaelJavi <ronnel.javier17@gmail.com>
2023-04-01 14:22:00 +0800

    Add router for customers.

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const books = require("../router/books");
 const departments = require("../router/departments");
+const customers = require("../router/customers");
 
 module.exports = function (app) {
 	app.use(express.json());
 	app.use("/api/books", books);
 	app.use("/api/departments", departments);
+	app.use("/api/customers", customers);
 };
