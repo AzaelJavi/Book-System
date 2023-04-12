e9d491899673cd055d391fc7e866d9b6f759c28c
AzaelJavi <ronnel.javier17@gmail.com>
2023-04-12 21:40:49 +0800

    added borrow routes.

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const books = require("../router/books");
 const departments = require("../router/departments");
 const customers = require("../router/customers");
+const borrows = require("../router/borrow");
 
 module.exports = function (app) {
 	app.use(express.json());
 	app.use("/api/books", books);
 	app.use("/api/departments", departments);
 	app.use("/api/customers", customers);
+	app.use("/api/borrows", borrows);
 };
