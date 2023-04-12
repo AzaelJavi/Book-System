045e4d9d36a36a9b85d4e2e4b6edfe7d634225bf
AzaelJavi <ronnel.javier17@gmail.com>
2023-04-12 21:40:31 +0800

    removed unneccessary codes and comments.

--- backend/models/customer-model.js
+++ backend/models/customer-model.js
@@ -11,6 +11,11 @@ const customerSchema = new mongoose.Schema({
 		trim: true,
 		minlength: 3,
 	},
+	studentNumber: {
+		type: String,
+		required: true,
+		trim: true,
+	},
 	address: {
 		type: String,
 		required: true,
@@ -26,26 +31,17 @@ const customerSchema = new mongoose.Schema({
 	books: {
 		type: [bookSchema],
 		default: [],
-		unique: true,
 	},
 });
 
 const Customer = mongoose.model("Customer", customerSchema);
-// TODO
-// Customer.aggregate([
-// 	{ $unwind: "books" },
-// 	{
-// 		$group: {
-// 			_id: "books.title",
-// 			count: { $sum: 1 },
-// 		},
-// 	},
-// 	{ $match: { count: { $gt: 1 } } },
-// ]);
 
 function validationJoi(req) {
 	const schema = Joi.object({
 		name: Joi.string().required().min(3),
+		studentNumber: Joi.string().length(9).required().messages({
+			"string.length": "Student Number must be exactly 9 characters long.",
+		}),
 		address: Joi.string().required().min(3),
 		phone: Joi.string().required().min(0).max(11),
 		bookId: Joi.objectId(),
--- backend/router/customers.js
+++ backend/router/customers.js
@@ -12,17 +12,18 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", [validate(validationJoi)], async (req, res) => {
-	const book = await Book.findById(req.body.bookId);
-	if (!book) return res.status(400).send("Invalid Book");
+	// const book = await Book.findById(req.body.bookId);
+	// if (!book) return res.status(400).send("Invalid Book");
 
 	const customer = new Customer({
 		name: req.body.name,
+		studentNumber: req.body.studentNumber,
 		address: req.body.address,
 		phone: req.body.phone,
-		books: {
-			id: book._id,
-			title: book.title,
-		},
+		// books: {
+		// 	id: book._id,
+		// 	title: book.title,
+		// },
 	});
 
 	await customer.save();
@@ -33,7 +34,7 @@ router.post("/", [validate(validationJoi)], async (req, res) => {
 router.put("/:id", validate(validationJoi), async (req, res) => {
 	const customer = await Customer.findByIdAndUpdate(
 		req.params.id,
-		_.pick(req.body, ["name", "address", "phone"]),
+		_.pick(req.body, ["name", "studentNumber", "address", "phone"]),
 		{ new: true }
 	);
 
@@ -42,34 +43,23 @@ router.put("/:id", validate(validationJoi), async (req, res) => {
 	res.send(customer);
 });
 
-//Books
-router.put("/:id/books", async (req, res) => {
-	const book = await Book.findById(req.body.bookId);
-	if (!book) return res.status(400).send("Invalid Book ID.");
+//Customer
 
-	const customer = await Customer.findOneAndUpdate(
-		{ _id: req.params.id },
-		{
-			$push: {
-				books: {
-					id: book._id,
-					title: book.title,
-				},
-			},
-		},
-		{ new: true }
-	);
+router.delete("/:id", async (req, res) => {
+	const customer = await Customer.findById(req.params.id);
 
-	if (!customer) return res.status(404).send("Invalid Customer.");
-	await customer.save();
+	if (!customer) return res.status(404).send("Customer ID not found.");
+
 	res.send(customer);
 });
 
+// Books
 router.delete("/:id/books/:bookId", async (req, res) => {
 	const { id, bookId } = req.params;
 
 	const customer = await Customer.findOne({ _id: id });
 
+	//req.body.id
 	customer.books.pull(bookId);
 
 	await customer.save();
