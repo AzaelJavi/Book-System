fe15e75c350c84b932149b629168527c2f9dc40f
AzaelJavi <ronnel.javier17@gmail.com>
2023-04-28 02:58:33 +0800

    Allow non-admin users temporary

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,4 +1,11 @@
+const config = require("config");
+
 module.exports = function (req, res, next) {
+	// Temporary
+	if (!config.get("requiresAuth")) return next();
+
+	// ----
+
 	if (!req.user.isAdmin) return res.status(403).send("Access denied.");
 
 	next();
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,6 +2,9 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
+	// Temporary
+	if (!config.get("requiresAuth")) return next();
+	// ---
 	const token = req.header("x-auth-token");
 	if (!token) return res.status(401).send("Access Denied. No token provided.");
 
