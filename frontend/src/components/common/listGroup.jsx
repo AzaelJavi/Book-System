4eb9149ba5448c813922bbb8e7c4652221bd87a6
AzaelJavi <ronnel.javier17@gmail.com>
2023-05-02 16:45:39 +0800

    feat: Added functionality for listgroup

--- frontend/src/components/common/listGroup.jsx
+++ frontend/src/components/common/listGroup.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 
-function ListGroup({ data }) {
+function ListGroup({ data, onSelectedDepartment, selectedDepartment }) {
+	let className =
+		"w-full cursor-pointer border-b-2 first:rounded-t-md last:rounded-b-md last:border-none p-4 text-2xl ring-1 ring-pink-500 md:text-xl transition duration-300";
 	return (
 		<React.Fragment>
 			<ul className="w-96 border-2 border-neutral-300 rounded-lg">
 				{data.map((item) => (
 					<li
+						onClick={() => onSelectedDepartment(item)}
 						key={item._id}
-						className="w-full cursor-pointer bg-primary-100 border-b-2 last:border-none p-4 text-2xl md:text-xl transition duration-500 hover:bg-neutral-100 hover:text-neutral-500">
+						className={
+							item === selectedDepartment
+								? `${className} bg-pink-300`
+								: `${className} hover:bg-pink-200`
+						}>
 						{item.name}
 					</li>
 				))}
