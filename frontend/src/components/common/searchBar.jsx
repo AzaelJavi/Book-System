6c3bc7cc1ec85c79b696f7b101e719925ae23b0e
AzaelJavi <ronnel.javier17@gmail.com>
2023-05-02 20:05:08 +0800

    feat: Added search box.

---
+++ frontend/src/components/common/searchBar.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+
+function SearchBar({ value, onSearch }) {
+	const searchBoxRef = useRef(null);
+
+	const handleClick = () => {
+		searchBoxRef.current.focus();
+	};
+	return (
+		<form>
+			<div className="relative">
+				<input
+					type="text"
+					ref={searchBoxRef}
+					className="sm:ml-6 p-3 w-4/6 rounded-l-md text-sm border-2 border-pink-500 focus:outline-none focus:ring-1 focus:ring-pink-500"
+					placeholder="Search..."
+					id="search-box"
+					value={value}
+					onChange={(e) => onSearch(e.currentTarget.value)}
+				/>
+				<button
+					type="submit"
+					onClick={handleClick}
+					class="absolute top-0 p-3.5 text-white bg-pink-700 rounded-r-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800">
+					<svg
+						aria-hidden="true"
+						class="w-5 h-5"
+						fill="none"
+						stroke="currentColor"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg">
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+					</svg>
+				</button>
+			</div>
+		</form>
+	);
+}
+
+export default SearchBar;
