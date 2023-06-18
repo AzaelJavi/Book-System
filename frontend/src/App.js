f095317ce8f0e32f1559662a86d8cddfffeff1d8
AzaelJavi <ronnel.javier17@gmail.com>
2023-06-18 22:03:09 +0800

    feat: Add which user on Navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Book from "./components/Book";
@@ -11,19 +11,31 @@ import BooksForm from "./components/booksForm";
 import CustomersForm from "./components/customersForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
+import auth from "./services/authService";
 
 function App() {
+	const [user, setUser] = useState("");
+
+	useEffect(() => {
+		const user = auth.getCurrentUser();
+		setUser(user);
+	}, []);
+
+	// console.log("User", user);
+
 	return (
 		<React.Fragment>
 			<ToastContainer />
-			<NavBar />
+			<NavBar user={user} />
 			<Routes>
 				<Route path="/login" element={<LoginForm />} />
+				<Route path="/logout" element={<Logout />} />
 				<Route path="/register" element={<RegisterForm />} />
 				<Route path="/books/:id" element={<BooksForm />} />
-				<Route path="/books" element={<Book />} />
+				<Route path="/books" element={<Book user={user} />} />
 				<Route path="/customers/:id" element={<CustomersForm />} />
-				<Route path="/customers" element={<Customer />} />
+				<Route path="/customers" element={<Customer user={user} />} />
 				<Route path="/borrows" element={<Borrow />} />
 				<Route path="/not-found" element={<NotFound />} />
 				<Route path="/" element={<Navigate to="/books" />} />
