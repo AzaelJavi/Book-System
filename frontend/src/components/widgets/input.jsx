91a10c8b429ab9bfeef24c809f1493a88f3b35ea
AzaelJavi <ronnel.javier17@gmail.com>
2023-07-15 22:39:49 +0800

    chore: removed unneccessary props.

--- frontend/src/components/widgets/input.jsx
+++ frontend/src/components/widgets/input.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Input({ label, name, isReadOnly, error, disabled, ...rest }) {
+function Input({ label, name, isReadOnly, error, ...rest }) {
 	return (
-		<div className="mb-3">
+		<>
 			<label
 				htmlFor={name}
 				className="block text-xl mb-2 font-medium text-gray-900">
@@ -15,14 +15,14 @@ function Input({ label, name, isReadOnly, error, disabled, ...rest }) {
 				{...rest}
 				className={`${error ? "border-red-500" : "border-gray-300"} ${
 					isReadOnly ? "cursor-not-allowed" : ""
-				} bg-gray-50 border rounded-lg focus:outline-none  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 text-xl`}
+				} bg-gray-50 border rounded-lg focus:outline-none  focus:ring-blue-500 focus:border-blue-500 block w-full p-3 text-xl`}
 			/>
 			{error && (
 				<div className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50">
 					{error}
 				</div>
 			)}
-		</div>
+		</>
 	);
 }
 
