dcff8e3777509d115b21c1401beee83057bdc027
AzaelJavi <ronnel.javier17@gmail.com>
2023-07-20 17:20:44 +0800

    preparing for deployment.

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ require("./startup/db")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || config.get("port");
 app.listen(port, () => console.log(`Listening on port ${port}...`));
