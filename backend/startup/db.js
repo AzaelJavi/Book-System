5964c263200c29ef67ce9258ec65c8346fc1020f
AzaelJavi <ronnel.javier17@gmail.com>
2023-07-21 20:32:39 +0800

    added dotenv npm to combine config npm for prod

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 module.exports = function () {
-	const db = config.get("db");
+	const db =
+		process.env.NODE_ENV === "production"
+			? process.env.PRODUCTION_DB
+			: config.get("db.development");
+
 	mongoose
 		.connect(db, { useNewUrlParser: true })
 		.then(() => console.log(`Connected to ${db}....`))
