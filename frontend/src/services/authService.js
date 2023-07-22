dac1957b9563d7605d79fde9b99bdd5f22b9a019
AzaelJavi <ronnel.javier17@gmail.com>
2023-07-22 22:16:18 +0800

    fix: call set JWT to auth service

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,6 +5,8 @@ import jwtDecode from "jwt-decode";
 const apiUrl = `${config.apiEndpoint}/auth`;
 const tokenKey = "token";
 
+http.setJwt(getJwt());
+
 export async function login(email, password) {
 	const { data: jwt } = await http.post(apiUrl, { email, password });
 	localStorage.setItem(tokenKey, jwt);
